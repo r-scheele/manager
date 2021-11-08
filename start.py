f56c427c49e9a892da30417dbceb6096e582e08f
r-scheele <67229938+Abdulrahman88-c@users.noreply.github.com>
2021-11-08 12:39:20 +0100

    crud with a json data format

--- start.py
+++ start.py
@@ -54,6 +54,23 @@ def save_input():
             password_form.delete(0, END)
 
 
+# Search password
+def search_password():
+    web = website_form.get().strip()
+    try:
+        with open("secure.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found!")
+    else:
+        try:
+            email, password = data[web]['email'], data[web]['password']
+            pyperclip.copy(password)
+            messagebox.showinfo(web, message=f"Username/Email:      {email} \nPassword:                 {password}")
+        except KeyError as error_message:
+            messagebox.showerror(web, message=f"Details for {error_message} not found!")
+
+
 # UI setup
 canvas = Canvas(width=200, height=200, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="password.png")
@@ -70,8 +87,8 @@ website_form = Entry(width=42)
 website_form.grid(column=1, row=2)
 website_form.focus()
 
-get_password_button = Button(text="Search", border=0, font=(FONT_NAME, 10))
-get_password_button.grid(column=2, row=2, pady=5, padx=10)
+search_button = Button(text="Search", width=16, border=0, font=(FONT_NAME, 10), command=search_password)
+search_button.grid(column=2, row=2)
 
 username_label = Label(text="Email/Username", bg=YELLOW, font=(FONT_NAME, 10))
 username_label.grid(column=0, row=3)
